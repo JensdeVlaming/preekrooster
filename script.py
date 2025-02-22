e358721ae011bba2447067054fc713ff794fc745
Jens de Vlaming <25007405+JensdeVlaming@users.noreply.github.com>
2025-02-22 20:49:14 +0100

    update script to google calendar

--- script.py
+++ script.py
@@ -1,42 +1,42 @@
-import os
-from time import sleep
-from datetime import datetime, timedelta
-from dotenv import load_dotenv
-from sqlalchemy import create_engine, text
-from O365 import Account, MSGraphProtocol
-import warnings
 import logging
-import schedule
+import os
+import sys
+import warnings
+from datetime import datetime, timedelta
+from time import sleep
+
 import requests
-import sys 
+import schedule
+from dotenv import load_dotenv
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from sqlalchemy import create_engine, text
 
 # Suppress specific warnings
-warnings.filterwarnings("ignore", message=".*The localize method is no longer necessary*")
-warnings.filterwarnings("ignore", message=".*The zone attribute is specific to pytz's interface*")
+warnings.filterwarnings(
+    "ignore", message=".*The localize method is no longer necessary*"
+)
+warnings.filterwarnings(
+    "ignore", message=".*The zone attribute is specific to pytz's interface*"
+)
 
 # Setup logger
-logger = logging.getLogger('my_logger')
-logger.setLevel(logging.DEBUG)  # Set the lowest level to capture all messages
+logger = logging.getLogger("preekrooster")
+logger.setLevel(logging.DEBUG)
 
-# Create handlers for both console and file
-console_handler = logging.StreamHandler(sys.stdout) # Logs to the stdout
-file_handler = logging.FileHandler('logfile.txt')  # Logs to a file
+console_handler = logging.StreamHandler(sys.stdout)
+file_handler = logging.FileHandler("logfile.txt")
 
-# Set the log level for each handler (can be different)
-console_handler.setLevel(logging.INFO)
-file_handler.setLevel(logging.DEBUG)
-
-# Create formatters and add them to the handlers
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+formatter = logging.Formatter("{asctime} - {levelname} - {message}", style="{")
 console_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
 
-# Add the handlers to the logger
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-# Load environment variables from .env file
+# Load environment variables
 load_dotenv()
+
 
 def load_env_variable(key, default=None):
     value = os.getenv(key)
@@ -45,40 +45,38 @@ def load_env_variable(key, default=None):
         raise ValueError(f"Missing {key}")
     return value
 
-# Get database connection details
-db_host = load_env_variable('DB_HOST')
-db_user = load_env_variable('DB_USER')
-db_password = load_env_variable('DB_PASSWORD')
-db_name = load_env_variable('DB_NAME')
-db_query = load_env_variable('DB_QUERY')
 
-# Get Azure / app credentials
-azure_client_id = load_env_variable('AZURE_CLIENT_ID')
-azure_secret_id = load_env_variable('AZURE_SECRET_ID')
-azure_tenant_id = load_env_variable('AZURE_TENANT_ID')
+# Database credentials
+db_host = load_env_variable("DB_HOST")
+db_user = load_env_variable("DB_USER")
+db_password = load_env_variable("DB_PASSWORD")
+db_name = load_env_variable("DB_NAME")
+db_query = load_env_variable("DB_QUERY")
 
-# Get Outlook details
-outlook_email = load_env_variable('OUTLOOK_EMAIL')
-outlook_calendar_name = load_env_variable('OUTLOOK_CALENDAR_NAME')
+# Google API credentials
+google_service_account_file = "./service_account.json"
+google_calendar_id = load_env_variable("GOOGLE_CALENDAR_ID")
 
-# Get Liturgie details
-liturgy_url = load_env_variable('LITURGY_URL')
+# Liturgy details
+liturgy_url = load_env_variable("LITURGY_URL")
 
-# Initialize Outlook Account
-protocol = MSGraphProtocol(api_version='v1.0')
-credentials = (azure_client_id, azure_secret_id)
-account = Account(credentials, auth_flow_type='credentials', tenant_id=azure_tenant_id, protocol=protocol)
+
+# Authenticate Google Calendar
+def get_google_calendar_service():
+    credentials = service_account.Credentials.from_service_account_file(
+        google_service_account_file, scopes=["https://www.googleapis.com/auth/calendar"]
+    )
+    return build("calendar", "v3", credentials=credentials)
+
 
 class Preekrooster:
-    
     @staticmethod
-    def create_outlook_event(row):
+    def create_google_calendar_event(row, existing_events):
         try:
-            schedule = account.schedule(resource=outlook_email)
-            calendar = schedule.get_calendar(calendar_name=outlook_calendar_name)
+            service = get_google_calendar_service()
 
             date = row[1]
-            time = datetime.strptime(row[2].strip().replace(".", ":"), '%H:%M').time()
+            time = datetime.strptime(row[2].strip().replace(".", ":"), "%H:%M").time()
 
             datetime_start = datetime.combine(date, time)
             datetime_end = datetime_start + timedelta(hours=1.5)
@@ -88,72 +86,95 @@ class Preekrooster:
             collecte1 = row[5].strip()
             collecte2 = row[6].strip()
             collecte3 = row[7].strip()
-            location = 'De Wijnstok'
+            location = "De Wijnstok"
 
             body = f"""
-            **Voorganger:**  
-            {predikant}  
-
-            **Collectedoelen:**  
-            1. {collecte1}  
-            2. {collecte2}  
-            3. {collecte3}  
-
-            **[Bekijk livestream](https://www.youtube.com/@pkndubbeldam/live)**
+            <strong>Voorganger:</strong> {predikant}<br /><br />
+            <strong>Collectedoelen:</strong><br />
+            1. {collecte1}<br />
+            2. {collecte2}<br />
+            3. {collecte3}<br />
+            <br />
+            <strong><a href="https://www.youtube.com/@pkndubbeldam/live">Bekijk livestream</a></strong>
             """
 
-            # Check for existing events in the given time range
-            events = Preekrooster.get_events_for_time_range(calendar, datetime_start, datetime_end)
+            print("checking for events with start time:", datetime_start.isoformat())
+
+            events = [
+                event
+                for event in existing_events
+                if event["start"]["dateTime"][:19] == datetime_start.isoformat()[:19]
+            ]
 
             if not events:
-                Preekrooster.create_new_event(calendar, subject, body, datetime_start, datetime_end, location)
+                Preekrooster.create_new_event(
+                    service, subject, body, datetime_start, datetime_end, location
+                )
             elif len(events) == 1:
-                Preekrooster.update_existing_event(events[0], subject, body, location, datetime_start)
+                Preekrooster.update_existing_event(
+                    service, events[0], subject, body, location, datetime_start
+                )
             else:
                 logger.warning("Multiple events found for the same time!")
                 for event in events:
                     logger.warning(str(event))
+
         except Exception as e:
             logger.error(f"Error in creating or updating event: {str(e)}")
 
     @staticmethod
-    def get_events_for_time_range(calendar, start, end):
-        query = calendar.new_query('start').greater_equal(start)
-        query.chain('and').on_attribute('end').less_equal(end)
-        events = calendar.get_events(query=query, include_recurring=True)
-        return list(events)
+    def get_events_for_time_range(service):
+        events_result = (
+            service.events()
+            .list(
+                calendarId=google_calendar_id,
+                singleEvents=True,
+            )
+            .execute()
+        )
+        return events_result.get("items", [])
 
     @staticmethod
-    def create_new_event(calendar, subject, body, start, end, location):
-        logger.info("Creating new event")
-        new_event = calendar.new_event()
-        new_event.subject = subject
-        new_event.body = body
-        new_event.start = start
-        new_event.end = end
-        new_event.location = location
-        new_event.save()
-        logger.info(f"New event created: {new_event}")
+    def create_new_event(service, subject, body, start, end, location):
+        logger.debug("Creating new event")
+        event = {
+            "summary": subject,
+            "location": location,
+            "description": body,
+            "start": {"dateTime": start.isoformat(), "timeZone": "Europe/Amsterdam"},
+            "end": {"dateTime": end.isoformat(), "timeZone": "Europe/Amsterdam"},
+        }
+        event = (
+            service.events().insert(calendarId=google_calendar_id, body=event).execute()
+        )
+        logger.debug(f"New event created: {event.get('htmlLink')}")
 
     @staticmethod
-    def update_existing_event(event, subject, body, location, datetime_start):
+    def update_existing_event(service, event, subject, body, location, datetime_start):
         logger.info("Updating existing event")
         if Preekrooster.is_in_current_week(datetime_start):
             liturgie = Preekrooster.get_liturgie(datetime_start)
             if liturgie:
-                body += """  
-                **[Druk hier voor de liturgie](https://www.pkndubbeldam.nl/files/liturgie.pdf)**  
+                body += """
+                <br />
+                <br />
+                <strong><a href="https://www.pkndubbeldam.nl/files/liturgie.pdf">Druk hier voor de liturgie</a></strong>
                 """
             else:
-                body += """ 
-                **Liturgie nog niet beschikbaar**  
+                body += """
+                <br />
+                <br />
+                <strong>Liturgie nog niet beschikbaar</strong>
                 """
-
-        event.subject = subject
-        event.body = body
-        event.location = location
-        event.save()
-        logger.info(f"Event updated: {event}")
+        event["summary"] = subject
+        event["description"] = body
+        event["location"] = location
+        updated_event = (
+            service.events()
+            .update(calendarId=google_calendar_id, eventId=event["id"], body=event)
+            .execute()
+        )
+        logger.debug(f"Event updated: {updated_event.get('htmlLink')}")
 
     @staticmethod
     def is_in_current_week(date):
@@ -167,7 +188,11 @@ class Preekrooster:
             days_to_subtract = event_date.weekday()
             first_date_of_week = event_date - timedelta(days=days_to_subtract)
 
-            headers = {'If-Modified-Since': first_date_of_week.strftime('%a, %d %b %Y %H:%M:%S GMT')}
+            headers = {
+                "If-Modified-Since": first_date_of_week.strftime(
+                    "%a, %d %b %Y %H:%M:%S GMT"
+                )
+            }
             response = requests.get(liturgy_url, headers=headers)
 
             if response.status_code == 200:
@@ -186,27 +211,40 @@ class Preekrooster:
     @staticmethod
     def get_rows_from_database():
         try:
-            db_engine = create_engine(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}")
+            db_engine = create_engine(
+                f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
+            )
             with db_engine.connect() as connection:
                 query = text(db_query)
-                rows = connection.execute(query)
-                return rows
+                return connection.execute(query).fetchall()
         except Exception as e:
             logger.error(f"Error fetching rows from database: {str(e)}")
             return []
 
     @staticmethod
+    def clear_calendar():
+        service = get_google_calendar_service()
+        events = service.events().list(calendarId=google_calendar_id).execute()
+        for event in events.get("items", []):
+            service.events().delete(
+                calendarId=google_calendar_id, eventId=event["id"]
+            ).execute()
+
+    @staticmethod
     def run():
-        logger.info("Running preekrooster job")
+        logger.debug("Running preekrooster job")
         database_rows = Preekrooster.get_rows_from_database()
-        account.authenticate()
+
+        service = get_google_calendar_service()
+
+        events = Preekrooster.get_events_for_time_range(service)
 
         for row in database_rows:
-            Preekrooster.create_outlook_event(row)
-        logger.info("Preekrooster job completed")
+            Preekrooster.create_google_calendar_event(row, events)
+        logger.debug("Preekrooster job completed")
+
 
 def main():
-    logger.info("Starting script")
     schedule.every().day.at("00:05").do(Preekrooster.run)
     schedule.every().sunday.at("09:30").do(Preekrooster.run)
 
@@ -214,6 +252,8 @@ def main():
         schedule.run_pending()
         sleep(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    # Preekrooster.clear_calendar()
     Preekrooster.run()
     main()
